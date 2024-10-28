56692dfc06dc9472e058219885f0b15debd2c4da
Hasan Al Mahadi <mahadi4uruetcse21@gmail.com>
2024-10-28 16:32:29 +0600

    1st commit

---
+++ app/src/main/java/com/temmahadi/healthcare/FindDoctorActivity.java
@@ -0,0 +1,59 @@
+package com.temmahadi.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class FindDoctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_doctor);
+
+        CardView exit= findViewById(R.id.Back);
+        exit.setOnClickListener(view -> {
+            startActivity(new Intent(FindDoctorActivity.this, HomeActivity.class));
+        });
+
+        CardView familyPhysician= findViewById(R.id.FDFamilyPhysician);
+        familyPhysician.setOnClickListener(view -> {
+            Intent it= new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title","Family Physician");
+            startActivity(it);
+        });
+
+        CardView dietitian= findViewById(R.id.FDDietitian);
+        dietitian.setOnClickListener(view -> {
+            Intent it= new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title","Dietitian");
+            startActivity(it);
+        });
+
+        CardView dentist= findViewById(R.id.FDDentist);
+        dentist.setOnClickListener(view -> {
+            Intent it= new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title","Dentist");
+            startActivity(it);
+        });
+
+        CardView surgeon= findViewById(R.id.FDSurgeon);
+        exit.setOnClickListener(view -> {
+            Intent it= new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title","Surgeon");
+            startActivity(it);
+        });
+
+        CardView cardiologist= findViewById(R.id.FDCardiologist);
+        exit.setOnClickListener(view -> {
+            Intent it= new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title","Cardiologist");
+            startActivity(it);
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/temmahadi/healthcare/HomeActivity.java
@@ -0,0 +1,37 @@
+package com.temmahadi.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedPreferences= getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username","");
+        Toast.makeText(getApplicationContext(),"Welcome "+username,Toast.LENGTH_SHORT).show();
+
+        CardView exit= findViewById(R.id.Exit);
+        exit.setOnClickListener(view -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear();
+            editor.apply();
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+        });
+
+        CardView findDoc= findViewById(R.id.FindDoc);
+        findDoc.setOnClickListener(view -> {
+            startActivity(new Intent(HomeActivity.this, FindDoctorActivity.class));
+        });
+    }
+}
